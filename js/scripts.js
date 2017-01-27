920bbafe06b569c8f5e2aa70e4ea955b13811815
Elise St Hilaire <eliseylenore@gmail.com>
2017-01-27 09:55:59 -0800

    create toppings div

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,26 @@ function Pizza(sizeChoice, toppings) {
   this.sizeChoice = sizeChoice,
   this.toppings = toppings
 }
-
+  var chosenPizza = new Pizza("",[]);
 
 
 //front-end logic
 $(document).ready(function() {
-  var chosenPizza = new Pizza("",[]);
+
 
   $("form#size-selection").submit(function(event) {
     event.preventDefault();
     chosenPizza.sizeChoice = $("input:radio[name=size]:checked").val();
-    console.log(chosenPizza.sizeChoice);
     $("#sizes").hide();
     $("#toppings").show();
   });
+
+  $("form#toppings-selection").submit(function(event) {
+    event.preventDefault();
+    chosenPizza.toppings = $("input:checkbox[name=toppings]:checked").val();
+    console.log(chosenPizza.toppings);
+    $("#toppings").hide();
+    $("#results").show();
+  });
+
 });
